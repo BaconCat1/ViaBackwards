88c96bb570b2ab59c820f82ee72442ccd29b2eaf
BaconCat__ <126538523+BaconCat1@users.noreply.github.com>
2026-06-26 00:03:09 -0500

    Fix: Use qualified inner interface ChunkTypeSupplier in BlockRewriter26_2 constructor

--- common/src/main/java/com/viaversion/viabackwards/protocol/v26_2to26_1/rewriter/BlockRewriter26_2.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v26_2to26_1/rewriter/BlockRewriter26_2.java
@@ -26,7 +26,7 @@ import com.viaversion.viaversion.api.rewriter.block.BlockRewriter1_21_5;
 
 public final class BlockRewriter26_2<C extends ClientboundPacketType> extends BlockRewriter1_21_5<C> {
 
-    public BlockRewriter26_2(final Protocol<C, ?, ?, ?> protocol, final ChunkTypeSupplier chunkTypeSupplier) {
+    public BlockRewriter26_2(final Protocol<C, ?, ?, ?> protocol, final BlockRewriter1_21_5.ChunkTypeSupplier chunkTypeSupplier) {
         super(protocol, chunkTypeSupplier);
     }
 
